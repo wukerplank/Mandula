dca8273cb2e45ce212f1238a65a9f6d409b90c7c
Christoph Edthofer <wukerplank@edthofer.at>
2013-03-13 12:45:49 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mandula::Application.config.secret_token = 'e835f7531ecc3e2ed154b2e785d0e39390e82fd47e6732859ff3f7c54b89c63bbead9fe7cea56a1d8d4a3f1630a4a2416fd1e12636b2c2d47e06731938abc3ac'
